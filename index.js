ad174906c596f13dae2d30b9cb6b1c0805fbcf32
Lauren Hess <laurenbethhess@gmail.com>
2021-11-02 17:46:57 -0400

    commit tree 6e6a7a80d2cc9f095e078da93f76c037afc0b571

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // Your code here
+
+// function saturdayFun(activity) {
+//     if (activity) {
+//         return `This Saturday, I want to ${activity}!`
+//     }
+//     else {
+//         return 'This Saturday, I want to roller-skate!'
+//     }
+// }
+
+function saturdayFun(activity='roller-skate') {
+    if (activity) {
+        return `This Saturday, I want to ${activity}!`
+    }
+}
+
+// function mondayWork(activity) {
+//     if (activity) {
+//         return `This Monday, I will ${activity}.`
+//     } else {
+        
+//         return 'This Monday, I will go to the office.'
+//     }
+// }
+
+function mondayWork(activity='go to the office') {
+    if (activity) {
+        return `This Monday, I will ${activity}.`
+    }
+}
+
+    // function wrapAdjective(visualFlair) {
+    //     if (visualFlair) {
+    //         return function (adjective) {
+    //             return `You are ${visualFlair}${adjective}${visualFlair}!`
+    //     }
+    //     } else {
+    //         let visualFlair = '*' 
+    //         return function (adjective) {
+    //             return `You are ${visualFlair}${adjective}${visualFlair}!`
+    //         }
+    //     }
+    // }
+
+function wrapAdjective(visualFlair='*') {
+    if (visualFlair) {
+        return function (adjective) {
+            return `You are ${visualFlair}${adjective}${visualFlair}!`
+        }
+    }
+}
